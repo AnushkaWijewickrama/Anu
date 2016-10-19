7c99a2cc3326dd7fe8bc721f64bfa6fb6d84811b
AnushkaWijewickrama <kushananushka12345@gmail.com>
2016-10-19 05:22:40 -0700

    Top.php
    top divistion

---
+++ Top.php
@@ -0,0 +1,34 @@
+# Anu
+<html>
+<head>
+
+<title> PHP web </title>
+
+
+</head>
+<a href="C:/Users/User/Downloads/samples/05_calendar/02_without_scheduler.html">cal</a>
+<img src="imghome.jpg" width="1332" height="275" alt="nanoict">
+<body  class="login course-1 notloggedin dir-ltr lang-en_utf8" id="login-index"
+
+<div id="page">
+    <div id="header" class=" clearfix" color="powder blue" >        <h1 class="headermain"><hr color="#0066FF">LMS - NANOICT<hr color="#0066FF"</h1></div></hr>
+        <div class="headermenu"><div class="logininfo">You are not logged in. (<a  href="login.php">Login</a>)</div></div>
+    </div>
+<div id="headerflashbase"></div>
+    <div id="headernormalpage"></div>
+
+<div id="menu"><br><br>
+
+
+<div id="menu">
+
+<a href="home.php"> Home </a> ||||
+<a href="a.php"> Computing</a> ||||
+<a href="b.php"> Enginnering </a> ||||
+<a href="c.php"> Managment </a> ||||
+</div>
+
+
+
+
+
